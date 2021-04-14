0d2c27d689867e46122185d1122befaa41704181
DaEunKim <ekdms717@naver.com>
2021-04-14 18:32:49 +0900

    first setting

---
+++ src/main/java/com/example/dani/DaniApplication.java
@@ -0,0 +1,15 @@
+package com.example.dani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaniApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DaniApplication.class, args);
+		System.out.println("test");
+	}
+
+}
